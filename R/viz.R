c8dfcfa0edcf64133ae7a44ab6c39d1fee8b65d7
Joachim Bargsten <jw@bargsten.org>
2015-10-01 01:36:01 +0200

    visualisation functions

--- R/viz.R
+++ R/viz.R
@@ -21,3 +21,27 @@ log_trans_shift <- function(base = exp(1), shift=0) {
 log10s1_trans <- function() {
     log_trans_shift(10, 0)
 }
+gg_annotate_boxplot_outliers <- function(g, data,
+  label.var="label",
+  value.var="value",
+  group.var="group",
+  ...) {
+    gb <- ggplot_build(g)$data[[1]]
+
+  lvls <- unique(levels(data[,group.var])[data[,group.var]])
+  data <- data[is.finite(data[,value.var]),]
+  outlier_labels <- gonz.merge_recurse(
+    lapply(seq_along(lvls), function(i) {
+      data.group <- data[data[,group.var]==lvls[i],] 
+      if(length(gb$outliers[[i]]) == 0)
+        return(NULL)
+      
+      data.frame( x=i, y=gb$outliers[[i]], label=data.group[data.group[,value.var] %in% gb$outliers[[i]], label.var])
+  }))
+  #browser()
+  if(nrow(outlier_labels) > 0) {
+    outlier_labels$pos <- ifelse(seq(1,nrow(outlier_labels)) %% 2 == 0, -0.1, 1.1)
+    g <- g + annotate("text", x=outlier_labels$x, y=outlier_labels$y, label=outlier_labels$label, hjust=outlier_labels$pos, ...)
+  }
+  g
+}
