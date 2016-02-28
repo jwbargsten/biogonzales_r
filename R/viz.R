f4bc63eba3f835e0576a9201dd551656601cbacb
Joachim Bargsten <jw@bargsten.org>
2016-02-28 15:59:43 +0100

    formatting

--- R/viz.R
+++ R/viz.R
@@ -1,23 +1,24 @@
 log_trans_shift <- function(base = exp(1), shift=0) {
-    trans <- function(x) {
-          y <- log(x + shift, base)
+  trans <- function(x) {
+    y <- log(x + shift, base)
     y[is.nan(y) | (is.infinite(y) & y < 0)] <- 0
-        y
-      }
+    y
+  }
   inv <- function(x) {
-        y <- base ^ x - shift
-      y[is.nan(y) | (is.infinite(y) & y < 0)] <- 0
-          y
-        }
-
-    lbr <- function(x) {
-          y <- log_breaks(base = base)(x+shift)
-        y[is.nan(y) | (is.infinite(y) & y < 0)] <- 0
-            y
-          }
-      trans_new(paste0("log-", format(base), "-", format(shift)), trans, inv,
-            lbr, domain = c(1+1e-100, Inf))
+    y <- base ^ x - shift
+    y[is.nan(y) | (is.infinite(y) & y < 0)] <- 0
+    y
+  }
+  lbr <- function(x) {
+    y <- log_breaks(base = base)(x+shift)
+    y[is.nan(y) | (is.infinite(y) & y < 0)] <- 0
+    y
+  }
+  trans_new(paste0("log-", format(base), "-", format(shift)), trans, inv,
+      lbr, domain = c(1+1e-100, Inf))
 }
 log10s1_trans <- function() {
     log_trans_shift(10, 0)
 }
+
+#ggplot(as.data.frame(z), aes(x=z)) + geom_histogram() + scale_y_continuous(trans=log_trans_shift(10,5))
