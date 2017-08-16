7642e6200d306b23eefead7b763f1f221c2e34ff
Joachim Bargsten <jw@bargsten.org>
2017-08-16 10:44:09 +0200

    also consider enviroment variables when init the log

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  if(file.exists("av")) {
+  if(nchar(Sys.getenv("ANALYSIS_VERSION")) > 0 || file.exists("av")) {
     gonz.log <- gonz.getlog()
     assign("gonz.log", gonz.log, envir = .GlobalEnv)
     gonz.log("info", paste0("invoked (", gonz.analysis_version(), ")", collapse=""))
