c62e78388ba21b5abc7ab9ee3f3a3a4d71a41078
Joachim Bargsten <jw@bargsten.org>
2024-08-29 10:00:44 +0200

    update to new way of doing things

--- R/analysis_version.R
+++ R/analysis_version.R
@@ -1,6 +1,63 @@
+.required_project_files <- c(".git", "docs", "analysis")
+.required_av_files <- c("av")
+
+.is.dir <- function(path) {
+  file.info(path)[1,"isdir"]
+}
+
+.abs2rel <- function(p, base) {
+  sub(base, "", p, fixed=TRUE)
+}
+
+.find_root <- function(start, req) {
+  current <- normalizePath(start)
+  if(!file.exists(current)) {
+    return(NA)
+  }
+  if(!.is.dir(current)) {
+    current <- dirname(current)
+  }
+
+  parent <- normalizePath(file.path(current, ".."))
+  while(parent != current) {
+    if(all(file.exists(file.path(current, req)))) {
+      return(current)
+    }
+    current <- parent
+    parent <- normalizePath(file.path(parent, ".."))
+  }
+  return(NA)
+}
+
+gonz.project_root <- function() .find_root(".", .required_project_files)
+gonz.analysis_root <- function() .find_root(".", .required_av_files)
+
+gonz.analysis_version <- function() {
+  av <- NULL
+  av.env <- Sys.getenv("ANALYSIS_VERSION")
+  av.src <- file.path(gonz.analysis_root(), "av")
+  if(nchar(av.env) > 0) {
+    av <- av.env
+  } else if(file.exists(av.src)) {
+    av <- as.character(read.table(av.src, as.is=TRUE)[1,1])
+  } else {
+    warning("could not find the analysis version file")
+    warning("using current dir as output dir")
+    av <- "."
+  }
+  av
+}
+
+gonz.analysis_path <- function() {
+  av.path <- file.path(gonz.analysis_root(), gonz.analysis_version())
+  if(!file.exists(av.path))
+    dir.create(av.path)
+  av.path
+}
+
 gonz.conf <- function(x=NULL, as.char=FALSE) {
   conf.file <- "gonz.conf.yml"
-  conf.av.file <- paste(gonz.analysis_version(), "conf", "yml", collapse=".", sep=".")
+  conf.av.file <- paste(gonz.analysis_path(), "conf", "yml", collapse=".", sep=".")
   gonz.log <- gonz.getlog()
   conf <- list()
 
@@ -17,18 +74,18 @@ gonz.conf <- function(x=NULL, as.char=FALSE) {
 
   conf <- rapply(conf,
     function(x) {
-      project_dir <- .find_root(getwd())
+      project_root <- gonz.project_root()
 
       x <- ifelse(grepl("^~[^/]*/", x), path.expand(x), x)
 
       x <- gsub("__av__", gonz.analysis_version(), x, fixed=TRUE)
       if(any(grepl("__path_to\\(([^)]+)\\)__", x) | grepl("__data__", x))) {
-        if(is.null(project_dir)) {
-          stop("could not find project dir")
+        if(is.na(project_root)) {
+          stop("could not find project root")
         } else {
-          gonz.log("info",paste0("project dir >>", project_dir, "<<", collapse=" "))
-          x <- gsub("__path_to\\(([^)]+)\\)__", file.path(project_dir, "\\1"), x)
-          x <- gsub("__data__", file.path(project_dir, "data"), x, fixed=TRUE)
+          gonz.log("info",paste0("project root >>", project_root, "<<", collapse=" "))
+          x <- gsub("__path_to\\(([^)]+)\\)__", file.path(project_root, "\\1"), x)
+          x <- gsub("__data__", file.path(project_root, "data"), x, fixed=TRUE)
         }
       }
 
@@ -51,32 +108,12 @@ gonz.conf <- function(x=NULL, as.char=FALSE) {
   }
 }
 
-gonz.analysis_version <- function() {
-  av <- NULL
-  av.env <- Sys.getenv("ANALYSIS_VERSION")
-  if(nchar(av.env) > 0) {
-    av <- av.env
-  } else if(file.exists("av")) {
-    av <- as.character(read.table("av", as.is=TRUE)[1,1])
-  } else {
-    warning("could not find the analysis version file")
-    warning("using current dir as output dir")
-    av <- "."
-  }
-  if(!file.exists(av))
-    dir.create(av)
-  av
-}
-
 gonz.nfi <- function(...) {
-  f <- file.path(gonz.analysis_version(), ...)
+  project_root <- gonz.project_root()
+  f <- file.path(gonz.analysis_path(), ...)
   gonz.log <- gonz.getlog()
-  gonz.log("info", paste0("(nfi) >", f, "<", collapse=" "))
+  gonz.log("info", paste0("(nfi) >", .abs2rel(f, project_root), "<", collapse=" "))
   f
-}
-
-.file.path.collapse <- function(x) {
-  paste0(x, collapse=.Platform$file.sep)
 }
 
 gonz.read_conf <- function(files) {
@@ -133,19 +170,6 @@ gonz.read_conf <- function(files) {
   }
 }
 
-.find_root <- function(dir) {
-  dir <- strsplit(dir, split="/")[[1]]
-  root <- NULL
-
-  while(length(dir) > 1) {
-    if(file.exists(.file.path.collapse(c(dir, "analysis"))) && file.exists(.file.path.collapse(c(dir, "Makefile"))) && file.exists(.file.path.collapse(c(dir, ".git")))) {
-      root <- dir
-      break
-    }
-    dir <- head(dir,-1)
-  }
-  .file.path.collapse(root)
-}
 
 gonz.av <- gonz.analysis_version
 gonz.c <- gonz.conf
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  if(nchar(Sys.getenv("ANALYSIS_VERSION")) > 0 || file.exists("av")) {
-    gonz.log <- gonz.getlog()
-    assign("gonz.log", gonz.log, envir = .GlobalEnv)
-    gonz.log("info", paste0("invoked (", gonz.analysis_version(), ")", collapse=""))
-  }
+  av <- gonz.analysis_version()
+  gonz.log <- gonz.getlog()
+  assign("gonz.log", gonz.log, envir = .GlobalEnv)
+  gonz.log("info", paste0("invoked (", gonz.analysis_version(), ")", collapse=""))
 }
